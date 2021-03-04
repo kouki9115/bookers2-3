cd9a5bea489c442821b49c2ab22be98c61c34cc6
kouki9115 <kouki.n9115@gmail.com>
2021-03-04 08:44:31 +0000

    Start-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books=Book.all
+    @book=Book.new
   end
 
   def edit
   end
+  
+  def new
+    @book=Book.new
+  end
+ 
+  def create
+    @books=Book.all
+    @book=Book.new(book_params)
+    @book.save
+    redirect_to books_path(@book.id)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  protect_from_forgery with: :null_session
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :books, dependent: :destroy
+  
+ validates :name, presence: true
+ validates :introduction, presence: true
 
 end
